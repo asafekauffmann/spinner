b282db63428af6ff60b97f3dcb0eeeb0f3bb46ce
Asafe kauffmann <dev.kauffmann@gmail.com>
2022-02-24 15:20:37 -0300

    up test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spinner';
+  constructor(private spinner: NgxSpinnerService) { }
+
+  ngOnInit(): void {
+
+    this.spinner.show();
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 5000);
+
+  }
 }
